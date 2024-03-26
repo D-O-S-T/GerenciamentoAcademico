68e09c23083a7449b9e32b200ef3dd6368e6d3ba
WIL-TZY <willowitzy@gmail.com>
2024-03-26 07:16:07 -0300

    Testagem na ClassePrincipal

--- src/IniciacaoCientifica/ClassePrincipal.java
+++ src/IniciacaoCientifica/ClassePrincipal.java
@@ -25,6 +25,23 @@ public class ClassePrincipal {
 		Bolsa bolsa1 = new Bolsa("Bolsa de Estudo", aluno1, false, 500.00);
 		sistema.solicitarBolsa(bolsa1);
 		
+		// Impressão dos dados cadastrados
+        System.out.println("Alunos cadastrados:");
+        List<Aluno> alunos = sistema.getAlunos();
+        for (Aluno aluno : alunos) {
+            System.out.println("Nome: " + aluno.getNome() + ", CPF: " + aluno.getCpf());
+        }
 
+        System.out.println("\nCursos cadastrados:");
+        List<Curso> cursos = sistema.getCursos();
+        for (Curso curso : cursos) {
+            System.out.println("Nome: " + curso.getNome() + ", Carga Horária: " + curso.getCargaHoraria());
+        }
+
+        System.out.println("\nBolsas solicitadas:");
+        List<Bolsa> bolsas = sistema.getBolsas();
+        for (Bolsa bolsa : bolsas) {
+            System.out.println("Tipo: " + bolsa.getTipo() + ", Aluno: " + bolsa.getAluno().getNome() + ", Valor: " + bolsa.getValor());
+        }
 	}
 }
